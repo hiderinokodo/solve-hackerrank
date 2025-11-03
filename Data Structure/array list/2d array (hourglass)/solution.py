2ddf3569bccd42adcb4c9b2034f389afb68877bc
hiderinokodo <hilihkinthil13@gmail.com>
2025-11-03 18:50:21 +0700

    add solution for ds section

---
+++ Data Structure/array list/2d array (hourglass)/solution.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hourglassSum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def hourglassSum(arr):
+    # Write your code here
+    n = len(arr)
+    ans = float('-inf')
+    
+    for i in range(n-2):
+        for j in range(n-2):
+            res = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+            if ans < res:
+                ans = res
+        
+    return ans
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+    
+    print(result)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
